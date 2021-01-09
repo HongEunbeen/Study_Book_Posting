2b9c1a865476b05ed74309d894dad5bc5cc97762
HongEunbeen <37995685+HongEunbeen@users.noreply.github.com>
2021-01-10 03:34:07 +0900

    commit 22 ~ 25 with test_code
    item22
    item22
    item23
    item24
    item25
    with test_code

---
+++ Effective_Java_3:E/Test_Code/Item22/PysicalConstants.java
@@ -0,0 +1,10 @@
+// interface PysicalConstants {
+//     static final double AVOGADROS_NUMGER = 6.022_140_857e23;
+// }
+
+public class PhysicalConstants {
+    private PhysicalConstatns() {}
+
+    static final double AVOGADROS_NUMGER = 6.022_140_857e23;
+
+}
