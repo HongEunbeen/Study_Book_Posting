9e6d75601757801c7e776adf203a95f23de75238
HongEunbeen <37995685+HongEunbeen@users.noreply.github.com>
2021-01-22 01:11:02 +0900

    Effective_java 예제 (item26 ~ item31)

--- Effective_Java_3:E/Test_Code/Item22/PysicalConstants.java
+++ Effective_Java_3:E/Test_Code/Item22/PysicalConstants.java
@@ -3,7 +3,7 @@
 // }
 
 public class PhysicalConstants {
-    private PhysicalConstatns() {}
+    private PhysicalConstatns() {}// 인스턴스 화 방지
 
     static final double AVOGADROS_NUMGER = 6.022_140_857e23;
 
